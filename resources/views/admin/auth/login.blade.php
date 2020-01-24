0315d89347ce4130233d87b09fb89ea60ba6f357
Jędrzej Buliński <git@jedrula.pl>
2020-01-24 22:47:46 +0100

    Nowa strona logowania

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -8,55 +8,58 @@
     <link rel="stylesheet" href="{{ mix('css/admin.css') }}">
 </head>
 <body>
-    <section class="hero is-fullheight">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-centered">
-                    <div class="column is-4">
+    <div class="columns is-marginless">
+        <div class="column is-half is-paddingless">
+            <div class="hero is-fullheight">
+                <div class="hero-body">
+                    <div class="container">
+                        <div class="columns is-centered">
+                            <div class="column is-8">
+                                <h1 class="title is-3">Logowanie</h1>
+                                <form method="post">
+                                    @csrf
 
-                        <div class="box">
-                            <h1 class="title is-3">Logowanie</h1>
-                            <form method="post">
-                                @csrf
-
-                                <div class="field">
-                                    <label class="label" for="error">E-mail</label>
-                                    <div class="control">
-                                        <input name="email" type="email" class="input @error('email') is-danger @enderror" required value="{{ old('email') }}" autofocus>
+                                    <div class="field">
+                                        <label class="label" for="error">E-mail</label>
+                                        <div class="control">
+                                            <input name="email" type="email" class="input @error('email') is-danger @enderror" required value="{{ old('email') }}" autofocus>
+                                        </div>
+                                        @error('email')
+                                            <p class="help is-danger">{{ $message }}</p>
+                                        @enderror
                                     </div>
-                                    @error('email')
-                                        <p class="help is-danger">{{ $message }}</p>
-                                    @enderror
-                                </div>
 
-                                <div class="field">
-                                    <label class="label" for="error">Hasło</label>
-                                    <div class="control">
-                                        <input name="password" type="password" class="input @error('password') is-danger @enderror" required value="{{ old('password') }}">
+                                    <div class="field">
+                                        <label class="label" for="error">Hasło</label>
+                                        <div class="control">
+                                            <input name="password" type="password" class="input @error('password') is-danger @enderror" required value="{{ old('password') }}">
+                                        </div>
+                                        @error('password')
+                                            <p class="help is-danger">{{ $message }}</p>
+                                        @enderror
                                     </div>
-                                    @error('password')
-                                        <p class="help is-danger">{{ $message }}</p>
-                                    @enderror
-                                </div>
 
-                                <div class="checkbox">
-                                    <label for="remember">
-                                        <input name="remember" id="remember" type="checkbox" class="checkbox">
-                                        Zapamiętaj mnie
-                                    </label>
-                                </div>
-                                <br><br>
+                                    <div class="checkbox">
+                                        <label for="remember">
+                                            <input name="remember" id="remember" type="checkbox" class="checkbox">
+                                            Zapamiętaj mnie
+                                        </label>
+                                    </div>
+                                    <br><br>
 
-                                <div class="buttons">
-                                    <button type="submit" class="button is-black">Zaloguj się</button>
-                                </div>
-                            </form>
+                                    <div class="buttons">
+                                        <button type="submit" class="button is-black">Zaloguj się</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="column is-half is-hidden-mobile is-paddingless">
+            <div class="hero is-fullheight has-image"></div>
+        </div>
+    </div>
 </body>
 </html>
