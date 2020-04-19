07fb76081a30bf54a44a6ec5214a6f9f631743cc
Bartosz Juraszewski <shadow38pl@gmail.com>
2020-04-20 01:18:32 +0200

    Testy

---
+++ database/factories/DepositFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Deposit;
+use Faker\Generator as Faker;
+use Bezhanov\Faker\ProviderCollectionHelper;
+
+$factory->define(Deposit::class, function (Faker $faker) {
+
+    ProviderCollectionHelper::addAllProvidersTo($faker);
+
+    return [
+        'quantity' => rand(0, 50),
+    ];
+});
