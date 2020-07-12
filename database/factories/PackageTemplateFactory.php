178334b474f30197539c95523b8d82249bb16fa2
Bartosz Juraszewski <shadow38pl@gmail.com>
2020-07-13 01:19:59 +0200

    Seeders fix, docs and single order endpoint

--- database/factories/PackageTemplateFactory.php
+++ database/factories/PackageTemplateFactory.php
@@ -11,7 +11,7 @@ $factory->define(PackageTemplate::class, function (Faker $faker) {
 
     return [
         'name' => $name . ' package',
-        'weight' => rand(1, 999) / 10.0,
+        'weight' => rand(1, 100) / 10.0,
         'width' => rand(1, 100),
         'height' => rand(1, 100),
         'depth' => rand(1, 100),
--- database/factories/ShippingMethodFactory.php
+++ database/factories/ShippingMethodFactory.php
@@ -7,7 +7,18 @@ use Faker\Generator as Faker;
 
 $factory->define(ShippingMethod::class, function (Faker $faker) {
     return [
-        'name' => $faker->company,
+        'name' => $faker->randomElement([
+            'dpd',
+            'fedex',
+            'ups',
+            'gls',
+            'poczta',
+            'inpost',
+            'inpostkurier',
+            'ruch',
+            'dhl',
+            'meest',
+        ]),
         'price' => rand(8, 15) + 0.99,
         'public' => rand(0, 1),
     ];
