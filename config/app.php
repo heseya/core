0e0f3d12954da71718c52e9b182ac7b4b85791d6
Jędrzej Buliński <git@jedrula.pl>
2021-01-15 21:24:16 +0100

    Remove Silverbox Client

--- config/app.php
+++ config/app.php
@@ -239,7 +239,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        // 'Facebook' => SammyK\LaravelFacebookSdk\FacebookFacade::class,
     ],
 
 ];
