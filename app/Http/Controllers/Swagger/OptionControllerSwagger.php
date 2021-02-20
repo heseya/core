9d94045e615c9c0e84ce39e91d5e2c2bf797be64
Jędrzej Buliński <git@jedrula.pl>
2021-02-20 08:13:31 +0100

    Option swagger

--- app/Http/Controllers/Swagger/OptionControllerSwagger.php
+++ app/Http/Controllers/Swagger/OptionControllerSwagger.php
@@ -2,7 +2,122 @@
 
 namespace App\Http\Controllers\Swagger;
 
+use App\Http\Requests\OptionStoreRequest;
+use App\Models\Option;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+
 interface OptionControllerSwagger
 {
+    /**
+     * @OA\Post(
+     *   path="/options",
+     *   tags={"Schemas"},
+     *   @OA\RequestBody(
+     *     @OA\JsonContent(
+     *       ref="#/components/schemas/Option"
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=201,
+     *     description="Created",
+     *     @OA\JsonContent(
+     *       @OA\Property(
+     *         property="data",
+     *         ref="#/components/schemas/Option"
+     *       )
+     *     )
+     *   ),
+     *   security={
+     *     {"oauth": {}}
+     *   }
+     * )
+     */
+    public function store(OptionStoreRequest $request): JsonResource;
 
+    /**
+     * @OA\Get(
+     *   path="/options/id:{id}",
+     *   tags={"Schemas"},
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(
+     *       type="string",
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Created",
+     *     @OA\JsonContent(
+     *       @OA\Property(
+     *         property="data",
+     *         ref="#/components/schemas/Option"
+     *       )
+     *     )
+     *   ),
+     *   security={
+     *     {"oauth": {}}
+     *   }
+     * )
+     */
+    public function show(Option $option): JsonResource;
+
+    /**
+     * @OA\Patch(
+     *   path="/options/id:{id}",
+     *   tags={"Schemas"},
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(
+     *       type="string",
+     *     )
+     *   ),
+     *   @OA\RequestBody(
+     *     @OA\JsonContent(
+     *       ref="#/components/schemas/Option"
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Created",
+     *     @OA\JsonContent(
+     *       @OA\Property(
+     *         property="data",
+     *         ref="#/components/schemas/Option"
+     *       )
+     *     )
+     *   ),
+     *   security={
+     *     {"oauth": {}}
+     *   }
+     * )
+     */
+    public function update(OptionStoreRequest $request, Option $option): JsonResource;
+
+    /**
+     * @OA\Delete(
+     *   path="/options/id:{id}",
+     *   tags={"Schemas"},
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(
+     *       type="string",
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=204,
+     *     description="Success",
+     *   ),
+     *   security={
+     *     {"oauth": {}}
+     *   }
+     * )
+     */
+    public function destroy(Option $option): JsonResponse;
 }
