1de35b05793819a6708cbe38d7a0399b8f1d35f1
Jędrzej Buliński <git@jedrula.pl>
2021-03-25 19:08:39 +0100

    Default settings

--- config/settings.php
+++ config/settings.php
@@ -2,9 +2,20 @@
 
 return [
 
-    'setting_name' => [
-        'value' => 'setting_value',
+    'bank_transfer_account' => [
+        'value' => '00 0000 0000 0000 0000 0000 0000',
         'public' => true,
     ],
-    
+
+    'bank_transfer_address' => [
+        'value' => env('APP_NAME'),
+        'public' => true,
+    ],
+
+
+    'dashboard_products_contain' => [
+        'value' => 0,
+        'public' => true,
+    ],
+
 ];
