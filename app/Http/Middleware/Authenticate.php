bd3dd65c9551e25ea85357ca7e8af47629ccaeac
Jędrzej Buliński <git@jedrula.pl>
2021-04-12 00:30:11 +0200

    Apps authorization

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\App;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Hash;
 
 class Authenticate extends Middleware
 {
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if (
+            $request->hasHeader('x-app-id') &&
+            $request->hasHeader('x-app-key') &&
+            $this->authenticateApp($request->header('x-app-id'), $request->header('x-app-key'))
+        ) {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next, ...$guards);
+    }
+
+    protected function authenticateApp($id, $key): bool
+    {
+        $app = App::find($id);
+
+        return $app && Hash::check($key, $app->key);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
      */
     protected function redirectTo($request)
     {
