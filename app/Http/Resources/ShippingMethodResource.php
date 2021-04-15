416c3b9af913bdc0678cf9d25e465d3220decbf2
Jędrzej Buliński <git@jedrula.pl>
2021-04-16 00:30:19 +0200

    Delivery price fix

--- app/Http/Resources/ShippingMethodResource.php
+++ app/Http/Resources/ShippingMethodResource.php
@@ -6,14 +6,23 @@ use Illuminate\Http\Request;
 
 class ShippingMethodResource extends Resource
 {
+    private ?float $fixedPrice = null;
+
     public function base(Request $request): array
     {
         return [
             'id' => $this->getKey(),
             'name' => $this->name,
-            'price' => $this->price,
+            'price' => $this->fixedPrice ?? $this->price,
             'public' => $this->public,
             'payment_methods' => PaymentMethodResource::collection($this->paymentMethods),
         ];
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->fixedPrice = $price;
+
+        return $this;
     }
 }
