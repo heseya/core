b31c0afce9dd090557ca965aa92500caf0c5cb2c
Jędrzej Buliński <git@jedrula.pl>
2021-04-25 20:01:29 +0200

    Swagger fix

--- app/Http/Controllers/CountriesController.php
+++ app/Http/Controllers/CountriesController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Swagger\CountryControllerSwagger;
 use App\Http\Resources\CountryResource;
 use App\Models\Country;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CountriesController extends Controller
+class CountriesController extends Controller implements CountryControllerSwagger
 {
     public function index(): JsonResource
     {
