8aed1c1af25d84af7b232b6d0fe0a42a98f175bf
Jędrzej Buliński <git@jedrula.pl>
2021-04-28 21:50:42 +0200

    PayPal link generation

--- app/Payments/PayPal.php
+++ app/Payments/PayPal.php
@@ -4,46 +4,30 @@ namespace App\Payments;
 
 use App\Models\Payment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use Srmklive\PayPal\Facades\PayPal as PayPalMethod;
 
 class PayPal implements PaymentMethod
 {
     public static function generateUrl(Payment $payment): array
     {
-        $client_id = config('paypal.client_id');
-        $secret = config('paypal.secret');
+        $provider = PayPalMethod::setProvider();
+        $provider->setApiCredentials(config('paypal'));
+        $provider->setAccessToken($provider->getAccessToken());
 
-//        $response = Http::withToken($client_id . ':' . $secret, 'Basic')
-//            ->withHeaders([
-//                'Accept' => 'application/json',
-//            ])
-//            ->asForm()
-//            ->post(config('paypal.url') . '/v1/oauth2/token', [
-//                'grant_type' => 'client_credentials'
-//            ]);
-//
-//        dd($response->body());
-
-        $response = Http::withToken(base64_encode($client_id . ':' . $secret), 'Basic')
-            ->withHeaders([
-                'Content-Type' => 'application/json',
-            ])
-            ->post(config('paypal.url') . '/v2/checkout/orders', [
-                'intent' => 'CAPTURE',
-                'purchase_units' => [
-                    [
-                        'amount' => [
-                            'currency_code' => $payment->order->currency,
-                            'value' => $payment->amount,
-                        ],
+        $response = $provider->createOrder([
+            'intent' => 'CAPTURE',
+            'purchase_units' => [
+                [
+                    'amount' => [
+                        'currency_code' => $payment->order->currency,
+                        'value' => $payment->amount,
                     ],
                 ],
-            ])->throw();
-
-        dd($response);
+            ],
+        ]);
 
         return [
-            'redirect_url' => $response['redirectUri'],
+            'redirect_url' => $response['links'][1]['href'],
         ];
     }
 
