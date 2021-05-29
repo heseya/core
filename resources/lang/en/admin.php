81b87652e97bcea092e9ab7b2af804fb6127eba2
Bartosz Juraszewski <shadow38pl@gmail.com>
2021-05-29 14:32:27 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -3,7 +3,7 @@
 return [
 
     'error' => [
-        'delete_with_relations' => 'Element can\'t be deleted, because has relations.',
+        'delete_with_relations' => 'Element can\'t be deleted, because it has relations.',
         'order_change_status_canceled' => 'Cannot change the status of a cancelled order',
     ],
 
