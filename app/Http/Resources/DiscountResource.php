7dd7c402de78c2fcf44c2db548b60d92029baf50
Jędrzej Buliński <git@jedrula.pl>
2021-06-07 00:27:27 +0200

    Discounts order calc fix

--- app/Http/Resources/DiscountResource.php
+++ app/Http/Resources/DiscountResource.php
@@ -8,12 +8,17 @@ class DiscountResource extends Resource
 {
     public function base(Request $request): array
     {
+        if (isset($this->resource->pivot)) {
+            $this->resource->type = $this->resource->pivot->type;
+            $this->resource->discount = $this->resource->pivot->discount;
+        }
+
         return [
             'id' => $this->getKey(),
             'code' => $this->code,
             'description' => $this->description,
-            'discount' => $this->discount,
-            'type' => $this->type,
+            'discount' => $this->resource->discount,
+            'type' => $this->resource->type,
             'uses' => $this->uses,
             'max_uses' => $this->max_uses,
             'available' => $this->available,
