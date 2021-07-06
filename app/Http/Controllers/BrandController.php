e5c20b762ec4f9660f1dfecb9a5679ee6ddf6d07
Jędrzej Buliński <git@jedrula.pl>
2021-07-06 09:10:18 +0200

    Delete brands

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\StoreException;
 use App\Http\Controllers\Swagger\BrandControllerSwagger;
-use App\Http\Requests\BrandCreateRequest;
 use App\Http\Requests\BrandIndexRequest;
-use App\Http\Requests\BrandOrderRequest;
-use App\Http\Requests\BrandUpdateRequest;
 use App\Http\Resources\BrandResource;
-use App\Models\Brand;
 use App\Models\ProductSet;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Response;
 
 class BrandController extends Controller implements BrandControllerSwagger
 {
+    /**
+     * @deprecated
+     */
     public function index(BrandIndexRequest $request): JsonResource
     {
         $query = ProductSet::where('slug', 'brands')->search($request->validated())->orderBy('order');
