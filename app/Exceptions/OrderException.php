1eeccefcdbce7168e7eba975553d7c316c307dca
Marek Kedzior <marek.kedzior@escola.pl>
2021-07-12 16:01:47 +0200

    fix insight

--- app/Exceptions/OrderException.php
+++ app/Exceptions/OrderException.php
@@ -2,12 +2,6 @@
 
 namespace App\Exceptions;
 
-use Throwable;
-
 class OrderException extends StoreException
 {
-    public function __construct(string $message = '', int $code = 422, ?Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
