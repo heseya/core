73ede6ccde9c00eff5debf959da46df5cd889d24
Marek Kedzior <marek.kedzior@escola.pl>
2021-07-26 14:24:20 +0200

    sort by order for pages

--- app/Services/PageService.php
+++ app/Services/PageService.php
@@ -24,7 +24,7 @@ class PageService implements PageServiceContract
             $query->where('public', true);
         }
 
-        return $query->paginate($itemsPerPage);
+        return $query->orderBy('order')->orderBy('updated_at')->paginate($itemsPerPage);
     }
 
     public function create(array $attributes): Page
