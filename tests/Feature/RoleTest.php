b8249a77b1169a24aa33152ea7bcb07fc20dedb6
Bartosz Juraszewski <shadow38pl@gmail.com>
2021-08-17 12:46:25 +0200

    Tests

--- tests/Feature/RoleTest.php
+++ tests/Feature/RoleTest.php
@@ -2,10 +2,8 @@
 
 namespace Tests\Feature;
 
-use App\Models\ProductSet;
-use App\Models\Product;
+use App\Models\Permission;
 use App\Models\Role;
-use App\Models\Schema;
 use Tests\TestCase;
 
 class RoleTest extends TestCase
@@ -81,5 +79,202 @@ class RoleTest extends TestCase
                 'assignable' => false,
                 'permissions' => [],
             ]]);
+    }
+
+    public function testShowPermissions(): void
+    {
+        $role = Role::create([
+            'name' => 'role1',
+            'description' => 'Role 1',
+        ]);
+
+        $permission1 = Permission::create(['name' => 'test.custom1']);
+        $permission2 = Permission::create(['name' => 'test.custom2']);
+        $role->syncPermissions([$permission1, $permission2]);
+
+        $response = $this->actingAs($this->user)->getJson('/roles/id:' . $role->getKey());
+
+        $response->assertOk()
+            ->assertJson(['data' => [
+                $role->getKeyName() => $role->getKey(),
+                'name' => $role->name,
+                'description' => $role->description,
+                'assignable' => false,
+                'permissions' => [
+                    'test.custom1',
+                    'test.custom2',
+                ],
+            ]]);
+    }
+
+    public function testCreateUnauthorized(): void
+    {
+        Permission::create(['name' => 'test.custom1']);
+        Permission::create(['name' => 'test.custom2']);
+
+        $response = $this->postJson('/roles', [
+            'name' => 'test_role',
+            'description' => 'Test role',
+            'permissions' => [
+                'test.custom1',
+                'test.custom2',
+            ],
+        ]);
+
+        $response->assertUnauthorized();
+    }
+
+    public function testCreate(): void
+    {
+        Permission::create(['name' => 'test.custom1']);
+        Permission::create(['name' => 'test.custom2']);
+
+        $response = $this->actingAs($this->user)->postJson('/roles', [
+            'name' => 'test_role',
+            'description' => 'Test role',
+            'permissions' => [
+                'test.custom1',
+                'test.custom2',
+            ],
+        ]);
+
+        $response->assertOk()
+            ->assertJson(['data' => [
+                'name' => 'test_role',
+                'description' => 'Test role',
+                'assignable' => false,
+                'permissions' => [
+                    'test.custom1',
+                    'test.custom2',
+                ],
+            ]]);
+
+        $role = Role::findByName('test_role');
+
+        $this->assertTrue($role->hasPermissionTo('test.custom1'));
+        $this->assertTrue($role->hasPermissionTo('test.custom2'));
+    }
+
+    public function testUpdateUnauthorized(): void
+    {
+        $role = Role::create([
+            'name' => 'role1',
+            'description' => 'Role 1',
+        ]);
+
+        $permission1 = Permission::create(['name' => 'test.custom1']);
+        $permission2 = Permission::create(['name' => 'test.custom2']);
+        Permission::create(['name' => 'test.custom3']);
+        $role->syncPermissions([$permission1, $permission2]);
+
+        $response = $this->actingAs($this->user)->patchJson('/roles/id:' . $role->getKey(), [
+            'name' => 'test_role',
+            'description' => 'Test role',
+            'permissions' => [
+                'test.custom2',
+                'test.custom3',
+            ],
+        ]);
+
+        $response->assertUnauthorized();
+    }
+
+    public function testUpdate(): void
+    {
+        $role = Role::create([
+            'name' => 'role1',
+            'description' => 'Role 1',
+        ]);
+
+        $permission1 = Permission::create(['name' => 'test.custom1']);
+        $permission2 = Permission::create(['name' => 'test.custom2']);
+        Permission::create(['name' => 'test.custom3']);
+        $role->syncPermissions([$permission1, $permission2]);
+
+        $response = $this->actingAs($this->user)->patchJson('/roles/id:' . $role->getKey(), [
+            'name' => 'test_role',
+            'description' => 'Test role',
+            'permissions' => [
+                'test.custom2',
+                'test.custom3',
+            ],
+        ]);
+
+        $response->assertOk()
+            ->assertJson(['data' => [
+                'name' => 'test_role',
+                'description' => 'Test role',
+                'assignable' => false,
+                'permissions' => [
+                    'test.custom2',
+                    'test.custom3',
+                ],
+            ]]);
+
+        $this->assertDatabaseHas('roles', [
+            $role->getKeyName() => $role->getKey(),
+            'name' => 'test_role',
+            'description' => 'Test role',
+        ]);
+
+        $role = Role::findByName('test_role');
+
+        $this->assertTrue($role->hasPermissionTo('test.custom2'));
+        $this->assertTrue($role->hasPermissionTo('test.custom3'));
+    }
+
+    public function testDeleteUnauthorized(): void
+    {
+        $role = Role::create([
+            'name' => 'role1',
+            'description' => 'Role 1',
+        ]);
+
+        $response = $this->actingAs($this->user)->deleteJson('/roles/id:' . $role->getKey());
+        $response->assertUnauthorized();
+    }
+
+    public function testDelete(): void
+    {
+        $role = Role::create([
+            'name' => 'role1',
+            'description' => 'Role 1',
+        ]);
+
+        $response = $this->actingAs($this->user)->deleteJson('/roles/id:' . $role->getKey());
+        $response->assertNoContent();
+
+        $this->assertDatabaseMissing('roles', [
+            $role->getKeyName() => $role->getKey(),
+        ]);
+    }
+
+    public function testDeletePermissions(): void
+    {
+        $role = Role::create([
+            'name' => 'role1',
+            'description' => 'Role 1',
+        ]);
+
+        $permission1 = Permission::create(['name' => 'test.custom1']);
+        $permission2 = Permission::create(['name' => 'test.custom2']);
+        $role->syncPermissions([$permission1, $permission2]);
+
+        $response = $this->actingAs($this->user)->deleteJson('/roles/id:' . $role->getKey());
+        $response->assertNoContent();
+
+        $this->assertDatabaseMissing('roles', [
+            $role->getKeyName() => $role->getKey(),
+        ]);
+
+        $this->assertDatabaseMissing('role_has_permissions', [
+            'role_id' => $role->getKey(),
+            'permission_id' => $permission1->getKey(),
+        ]);
+
+        $this->assertDatabaseMissing('role_has_permissions', [
+            'role_id' => $role->getKey(),
+            'permission_id' => $permission2->getKey(),
+        ]);
     }
 }
