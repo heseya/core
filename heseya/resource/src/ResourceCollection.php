3e93b74e74c95c6fc702f39834359cf20d6d96a8
Jędrzej Buliński <git@jedrula.pl>
2021-08-26 18:26:02 +0200

    HES-203

---
+++ heseya/resource/src/ResourceCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Heseya\Resource;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class ResourceCollection extends AnonymousResourceCollection
+{
+    protected bool $full = false;
+
+    public function full(bool $full = true): self
+    {
+        $this->full = $full;
+
+        return $this;
+    }
+
+    /**
+     * Transform the resource into a JSON array.
+     *
+     * @param Request $request
+     */
+    public function toArray($request): array
+    {
+        return $this->collection->each(fn ($el) => $el->isIndex(!$this->full)->toArray($request))->all();
+    }
+}
