4f8dfdb16f0d25a588642c220e10d7eaf461cda5
Bartosz Juraszewski <shadow38pl@gmail.com>
2021-09-02 10:56:35 +0200

    Test fix

--- app/SearchTypes/ProductSearch.php
+++ app/SearchTypes/ProductSearch.php
@@ -13,7 +13,17 @@ class ProductSearch extends Search
             $query->where('id', 'LIKE', '%' . $this->value . '%')
                 ->orWhere('slug', 'LIKE', '%' . $this->value . '%')
                 ->orWhere('name', 'LIKE', '%' . $this->value . '%')
-                ->orWhere('description_html', 'LIKE', '%' . $this->value . '%');
+                ->orWhere('description_html', 'LIKE', '%' . $this->value . '%')
+                ->orWhereHas('brand', function (Builder $query): void {
+                    $query
+                        ->where('name', 'LIKE', '%' . $this->value . '%')
+                        ->orWhere('slug', 'LIKE', '%' . $this->value . '%');
+                })
+                ->orWhereHas('category', function (Builder $query): void {
+                    $query
+                        ->where('name', 'LIKE', '%' . $this->value . '%')
+                        ->orWhere('slug', 'LIKE', '%' . $this->value . '%');
+                });
         });
     }
 }
