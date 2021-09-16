08230b3de99acbb1131ebcde75f03d54ff2c02ef
Jędrzej Buliński <git@jedrula.pl>
2021-09-16 20:24:02 +0200

    fix

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -6,13 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function (): void {
     Route::post('logout', [AuthController::class, 'logout'])
         ->middleware('auth:api');
-    Route::get('login-history', [AuthController::class, 'loginHistory'])
-        ->middleware('can:auth.sessions.show');
-    Route::get('kill-session/id:{id}', [AuthController::class, 'killActiveSession'])
-        ->middleware('can:auth.sessions.revoke');
-    Route::get('kill-all-sessions', [AuthController::class, 'killAllSessions'])
-        ->middleware('can:auth.sessions.revoke');
+//    Route::get('login-history', [AuthController::class, 'loginHistory'])
+//        ->middleware('can:auth.sessions.show');
+//    Route::get('kill-session/id:{id}', [AuthController::class, 'killActiveSession'])
+//        ->middleware('can:auth.sessions.revoke');
+//    Route::get('kill-all-sessions', [AuthController::class, 'killAllSessions'])
+//        ->middleware('can:auth.sessions.revoke');
+    Route::post('refresh', [AuthController::class, 'refresh'])
+        ->middleware('can:auth.login');
     Route::get('profile', [AuthController::class, 'profile']);
+    Route::get('profile/{identity_token}', [AuthController::class, 'identityProfile'])
+        ->middleware('can:auth.identity_profile');
 });
 
 Route::post('login', [AuthController::class, 'login'])
