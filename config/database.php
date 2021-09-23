013e3eaa7863c932c6fd45953811f6dcd809bb47
Jędrzej Buliński <git@jedrula.pl>
2021-09-23 08:41:54 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- config/database.php
+++ config/database.php
@@ -60,9 +60,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone = \'+00:00\'',
             ]) : [],
-            'timezone' => 'Europe/Warsaw'
         ],
 
         'pgsql' => [
