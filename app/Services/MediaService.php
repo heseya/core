9025bdd74ff765e9d69b75104161c4ba9498326b
Jędrzej Buliński <git@jedrula.pl>
2021-10-17 16:37:33 +0200

    HES-317

--- app/Services/MediaService.php
+++ app/Services/MediaService.php
@@ -28,8 +28,7 @@ class MediaService implements MediaServiceContract
 
     public function store(UploadedFile $file): Media
     {
-        $response = Http::attach('file', $file
-            ->getContent(), 'file')
+        $response = Http::attach('file', $file->getContent(), 'file')
             ->withHeaders(['x-api-key' => config('silverbox.key')])
             ->post(config('silverbox.host') . '/' . config('silverbox.client'));
 
@@ -43,6 +42,15 @@ class MediaService implements MediaServiceContract
         ]);
     }
 
+    private function getMediaType(string $extension): int
+    {
+        return match($extension) {
+            'jpeg', 'jpg', 'png', 'gif', 'bmp', 'svg' => MediaType::PHOTO,
+            'mp4', 'webm' => MediaType::VIDEO,
+            default => MediaType::OTHER,
+        };
+    }
+
     public function destroy(Media $media): void
     {
         if ($media->products()->exists()) {
@@ -50,18 +58,5 @@ class MediaService implements MediaServiceContract
         }
 
         $media->forceDelete();
-    }
-
-    private function getMediaType(string $extension): int
-    {
-        $imageExtensions = ['jpeg', 'png', 'gif', 'bmp', 'svg'];
-        $videoExtensions = ['mp4', 'webm'];
-        if (in_array($extension, $imageExtensions)) {
-            return MediaType::PHOTO;
-        }
-        if (in_array($extension, $videoExtensions)) {
-            return MediaType::VIDEO;
-        }
-        return MediaType::OTHER;
     }
 }
