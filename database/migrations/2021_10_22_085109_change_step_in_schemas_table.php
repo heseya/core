592a10429157300bf017fdd4d0cea417b0750ecb
Jędrzej Buliński <git@jedrula.pl>
2021-10-22 10:53:42 +0200

    HES-376

---
+++ database/migrations/2021_10_22_085109_change_step_in_schemas_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeStepInSchemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('schemas', function (Blueprint $table) {
+            $table->unsignedDecimal('step', 12, 8)->change();
+        });
+    }
+}
