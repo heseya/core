95e5f8a134381b81c0aaa8911d90c6906b70f2f8
Jędrzej Buliński <git@jedrula.pl>
2021-11-29 17:30:06 +0000

    Merge branch 'feature/HES-546' into 'develop'
    Error fix and test
    
    See merge request heseya/store-api!288

---
+++ tests/Unit/SchemaValidationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Enums\SchemaType;
+use App\Models\Schema;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SchemaValidationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testValidateSelectSchemaOptional(): void
+    {
+        /** @var Schema $schema */
+        $schema = Schema::factory()->create([
+            'type' => SchemaType::SELECT,
+            'required' => false,
+        ]);
+
+        $this->assertNull(
+            $schema->validate(null, 1),
+        );
+    }
+}
