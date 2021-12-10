72e4d0e11c6e8ebe4e24ce06a8ea31f85fa35149
Jędrzej Buliński <git@jedrula.pl>
2021-12-10 09:52:07 +0100

    Style fix

--- app/Services/MediaService.php
+++ app/Services/MediaService.php
@@ -42,15 +42,6 @@ class MediaService implements MediaServiceContract
         ]);
     }
 
-    private function getMediaType(string $extension): int
-    {
-        return match ($extension) {
-            'jpeg', 'jpg', 'png', 'gif', 'bmp', 'svg', 'webp' => MediaType::PHOTO,
-            'mp4', 'webm' => MediaType::VIDEO,
-            default => MediaType::OTHER,
-        };
-    }
-
     public function destroy(Media $media): void
     {
         if ($media->products()->exists()) {
@@ -58,5 +49,14 @@ class MediaService implements MediaServiceContract
         }
 
         $media->forceDelete();
+    }
+
+    private function getMediaType(string $extension): int
+    {
+        return match ($extension) {
+            'jpeg', 'jpg', 'png', 'gif', 'bmp', 'svg', 'webp' => MediaType::PHOTO,
+            'mp4', 'webm' => MediaType::VIDEO,
+            default => MediaType::OTHER,
+        };
     }
 }
