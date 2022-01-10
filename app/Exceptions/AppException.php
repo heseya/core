d82743b9e400b99d98679d9e3f0419ce144c04c1
Bartosz Juraszewski <shadow38pl@gmail.com>
2022-01-10 14:20:54 +0100

    New app errors with response body

--- app/Exceptions/AppException.php
+++ app/Exceptions/AppException.php
@@ -9,8 +9,14 @@ class AppException extends StoreException
     public function __construct(
         string $message = '',
         int $code = 0,
-        ?Throwable $previous = null
+        ?Throwable $previous = null,
+        private array $errorArray = [],
     ) {
         parent::__construct($message, $code, $previous);
+    }
+
+    public function errors(): array
+    {
+        return $this->errorArray;
     }
 }
