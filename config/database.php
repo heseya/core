09980f1fef07d33ce9153261a327ef7f1b6f2642
Jędrzej Buliński <git@jedrula.pl>
2022-02-01 14:15:08 +0100

    Redis schema path

--- config/database.php
+++ config/database.php
@@ -106,19 +106,23 @@ return [
         ],
 
         'default' => [
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'path' => env('REDIS_PATH', ''),
         ],
 
         'cache' => [
-            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'path' => env('REDIS_PATH', ''),
         ],
 
     ],
