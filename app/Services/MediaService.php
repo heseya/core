daf04e0376842b258110aef845183487e15bab25
Dawid Miklas <dawid.miklas@escolasoft.com>
2022-02-08 07:31:47 +0100

    HES-491

--- app/Services/MediaService.php
+++ app/Services/MediaService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Dtos\MediaUpdateDto;
 use App\Enums\MediaType;
 use App\Exceptions\AppAccessException;
+use App\Exceptions\MediaCriticalException;
 use App\Models\Media;
 use App\Models\Product;
 use App\Services\Contracts\MediaServiceContract;
@@ -36,7 +37,7 @@ class MediaService implements MediaServiceContract
             ->post(Config::get('silverbox.host') . '/' . Config::get('silverbox.client'));
 
         if ($response->failed()) {
-            throw new AppAccessException('CDN responded with an error', 500);
+            throw new MediaCriticalException('CDN responded with an error', 500);
         }
 
         return Media::create([
