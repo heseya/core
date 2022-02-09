befe87be72ef5336d71a68484502c8a201380f6c
Mariusz Baranowski <mariusz.baranowski@escolasoft.com>
2022-02-09 13:29:32 +0100

    feature/HES-353, added trait which compact all translations

---
+++ app/Traits/GetAllTranslations.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+trait GetAllTranslations
+{
+    /**
+     * Returns all available translations for user
+     *
+     * @return array
+     * */
+    protected function getAllTranslations(): array
+    {
+        $allTranslations = [];
+        $languages = $this->published;
+
+        foreach($this->getTranslations(allowedLocales: $languages) as $field => $translations){
+            foreach($translations as $locale => $translation){
+                if(!array_key_exists($locale, $allTranslations)) {
+                    $allTranslations[$locale] = [];
+                }
+
+                $allTranslations[$locale] += [$field => $translation];
+            }
+        }
+
+        return ['translations' => $allTranslations];
+    }
+}
