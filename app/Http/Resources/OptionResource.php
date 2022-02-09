c360ee7f6633829c25b2ab06fc2e9c08b3ef66db
Mariusz Baranowski <mariusz.baranowski@escolasoft.com>
2022-02-09 13:31:28 +0100

    feature/HES-353, handle translation flag at Option resource

--- app/Http/Resources/OptionResource.php
+++ app/Http/Resources/OptionResource.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Resources;
 
+use App\Traits\GetAllTranslations;
 use Illuminate\Http\Request;
 
 class OptionResource extends Resource
 {
+    use GetAllTranslations;
+
     public function base(Request $request): array
     {
-        return [
+        $data = [
             'id' => $this->getKey(),
             'name' => $this->name,
             'price' => $this->price,
@@ -16,5 +19,7 @@ class OptionResource extends Resource
             'available' => $this->available,
             'items' => ItemResource::collection($this->items),
         ];
+
+        return array_merge($data, array_key_exists('translations', $request->toArray()) ? $this->getAllTranslations() : []);
     }
 }
