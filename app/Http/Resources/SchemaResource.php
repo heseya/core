7295ec2ca64e6449020c93ea7a521904ff0e8fb7
Mariusz Baranowski <mariusz.baranowski@escolasoft.com>
2022-02-09 13:30:34 +0100

    feature/HES-353, handle translation flag at Product resource

--- app/Http/Resources/SchemaResource.php
+++ app/Http/Resources/SchemaResource.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Resources;
 
+use App\Traits\GetAllTranslations;
 use Illuminate\Http\Request;
 
 class SchemaResource extends Resource
 {
+    use GetAllTranslations;
+
     public function base(Request $request): array
     {
-        return [
+        $data =  [
             'id' => $this->getKey(),
             'type' => $this->type->key,
             'name' => $this->name,
@@ -26,6 +29,8 @@ class SchemaResource extends Resource
             'options' => OptionResource::collection($this->options),
             'used_schemas' => $this->usedSchemas->map(fn ($schema) => $schema->getKey()),
         ];
+
+        return array_merge($data, array_key_exists('translations', $request->toArray()) ? $this->getAllTranslations() : []);
     }
 
     public function view(Request $request): array
