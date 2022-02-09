ca11f7d34b2fc92e06b51f052dff33eb570aefb9
Bartosz Juraszewski <shadow38pl@gmail.com>
2022-02-09 23:47:26 +0100

    Add missing changes

--- app/Http/Requests/PageStoreRequest.php
+++ app/Http/Requests/PageStoreRequest.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\Translations;
+
 class PageStoreRequest extends SeoMetadataRulesRequest
 {
     public function rules(): array
     {
         return $this->rulesWithSeo([
-            'name' => ['required', 'string', 'max:255'],
+            'translations' => [
+                new Translations(['name', 'content_html']),
+            ],
+//            'name' => ['required', 'string', 'max:255'],
+//            'content_html' => ['required', 'string', 'min:1'],
+
+            'published' => ['required', 'array', 'min:1'],
+            'published.*' => ['uuid', 'exists:languages,id'],
+
             'slug' => ['required', 'string', 'max:255'],
             'public' => ['boolean'],
-            'content_html' => ['required', 'string', 'min:1'],
         ]);
     }
 }
