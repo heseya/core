fa2221cbb705a047a38d7bab0d0aa3e4e1e0a6c1
Mariusz Baranowski <mariusz.baranowski@escolasoft.com>
2022-02-15 11:47:38 +0100

    feature/HES-840 adding new option to attribute

--- app/Services/AttributeOptionService.php
+++ app/Services/AttributeOptionService.php
@@ -8,16 +8,21 @@ use App\Services\Contracts\AttributeOptionServiceContract;
 
 class AttributeOptionService implements AttributeOptionServiceContract
 {
-    public function updateOrCreate(string $attributeId, AttributeOptionDto $dto): AttributeOption
+    public function create(string $attributeId, AttributeOptionDto $dto): AttributeOption
     {
         $data = array_merge(
             ['attribute_id' => $attributeId],
             $dto->toArray(),
         );
 
+        return AttributeOption::create($data);
+    }
+
+    public function updateOrCreate(string $attributeId, AttributeOptionDto $dto): AttributeOption
+    {
         $this->deleteAttributeOptions($attributeId);
 
-        return AttributeOption::create($data);
+        return $this->create($attributeId, $dto);
     }
 
     public function deleteAttributeOptions(string $attributeId): void
