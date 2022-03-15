d83fabea2f0b22a0f13f5bde8b4cd7ba200da9a1
Dawid Miklas <dawid.miklas@escolasoft.com>
2022-03-15 12:21:19 +0100

    created migration for new columns in orders table

---
+++ database/migrations/2022_03_15_100005_created_shipping_place_id_invoice_requested_shipping_type_columns_in_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('shipping_place')->nullable();
+            $table->boolean('invoice_requested')->default(false);
+            $table->string('shipping_type')->default('none');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('shipping_place');
+            $table->dropColumn('invoice_requested');
+            $table->dropColumn('shipping_type');
+        });
+    }
+};
