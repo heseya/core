7c65498654f103ea0ad4e8f033bfb6ff2a9f1e59
Dawid Miklas <dawid.miklas@escolasoft.com>
2022-03-17 10:07:45 +0100

    OrderResource change to return shipping_place as id

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -25,7 +25,7 @@ class OrderResource extends Resource
             'shipping_address' => $this->shippingAddress ? AddressResource::make($this->shippingAddress) : null,
             'shipping_method' => $this->shippingMethod ? ShippingMethodResource::make($this->shippingMethod) : null,
             'invoice_requested' => $this->invoice_requested,
-            'shipping_place' => AddressResource::make(Address::find($this->shipping_place)) ?? $this->shipping_place,
+            'shipping_place' => $this->shipping_place,
         ];
     }
 
