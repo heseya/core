3c3c9206333b8cd414f3976a7368472501688b99
Jędrzej Buliński <git@jedrula.pl>
2022-03-21 22:40:25 +0100

    Composer update

--- app/Models/ItemProduct.php
+++ app/Models/ItemProduct.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * @mixin IdeHelperItemProduct
+ */
 class ItemProduct extends Pivot
 {
     protected $casts = [
