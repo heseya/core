e9f7f84a41778dddccbb0398261dba52caaa7ce5
Krzysztof Dziedziech <krzysztof.dziedziech@escolasoft.com>
2022-04-28 10:49:19 +0200

    fix

--- app/Services/SettingsService.php
+++ app/Services/SettingsService.php
@@ -14,7 +14,7 @@ class SettingsService implements SettingsServiceContract
     {
         $settings = Setting::orderBy('name')->get();
 
-        /** @var Collection<int, mixed> $settings */
+        /** @var Collection<int, mixed> $configSettings */
         $configSettings = Config::get('settings');
 
         Collection::make($configSettings)->each(function ($setting, $key) use ($settings): void {
