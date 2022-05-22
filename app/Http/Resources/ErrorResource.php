64af3f56ede70ece6108fca509bef38cfa3e5286
Jędrzej Buliński <git@jedrula.pl>
2022-05-22 14:58:53 +0000

    HES-1378

--- app/Http/Resources/ErrorResource.php
+++ app/Http/Resources/ErrorResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 
 final class ErrorResource extends Resource
 {
@@ -13,11 +14,18 @@ final class ErrorResource extends Resource
 
     public function base(Request $request): array
     {
+        $stack = [];
+
+        // Show stack when app debug is on
+        if (Config::get('app.debug') === true) {
+            $stack = ['stack' => $this->resource->stack];
+        }
+
         return [
             'message' => $this->resource->message,
             'code' => $this->resource->code,
             'key' => $this->resource->key,
             'errors' => $this->resource->errors,
-        ];
+        ] + $stack;
     }
 }
