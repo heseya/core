de344cf337461ab8ee6b9d21b782d3bfb1aa3bcf
Dawid Miklas <dawid.miklas@escolasoft.com>
2022-06-30 11:25:48 +0200

    has_schema handling fix

--- app/Observers/SchemaObserver.php
+++ app/Observers/SchemaObserver.php
@@ -10,7 +10,7 @@ class SchemaObserver
     public function deleting(Schema $schema): void
     {
         $schema->products->each(function (Product $product) use ($schema): void {
-            $productSchemas = $product->schemas->where('id', '!=', $schema->id);
+            $productSchemas = $product->schemas()->where('id', '!=', $schema->id)->get();
             if ($productSchemas->isEmpty()) {
                 $product->update(['has_schemas' => false]);
             }
