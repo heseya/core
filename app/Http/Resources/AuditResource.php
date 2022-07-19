9599ba231b8f969dd3931b034eecb668f55b0bd0
Jędrzej Buliński <git@jedrula.pl>
2022-07-19 09:48:27 +0200

    HES-1598

--- app/Http/Resources/AuditResource.php
+++ app/Http/Resources/AuditResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AuditResource extends Resource
 {
@@ -14,7 +16,10 @@ class AuditResource extends Resource
             'created_at' => $this->resource->created_at,
             'old_values' => $this->resource->old_values,
             'new_values' => $this->resource->new_values,
-            'user' => UserResource::make($this->resource->user)->baseOnly(),
+            'issuer_type' => Str::of($this->resource->user_type)->after('App\\Models\\')->lower(),
+            'issuer' => $this->resource->user instanceof User ?
+                UserResource::make($this->resource->user)->baseOnly() :
+                AppResource::make($this->resource->user)->baseOnly(),
         ];
     }
 }
