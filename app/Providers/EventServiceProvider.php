ae605cacad4547ca78f02cc1661dee5162565a02
Jędrzej Buliński <git@jedrula.pl>
2022-07-20 10:11:52 +0200

    Style fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -64,7 +64,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The event listener mappings for the application.
      *
-     * @var array
+     * @var array<class-string, array<class-string>>
      */
     protected $listen = [
         OrderCreated::class => [
@@ -88,7 +88,7 @@ class EventServiceProvider extends ServiceProvider
     ];
 
     /**
-     * Webhooks events.
+     * @var array<class-string>
      */
     private array $webhookEvents = [
         CouponCreated::class,
@@ -131,12 +131,15 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         parent::boot();
+
         Event::listen($this->webhookEvents, WebHookEventListener::class);
-        Payment::observe(PaymentObserver::class);
-        Deposit::observe(DepositObserver::class);
+
+        // Ugly observers 🤮
         AttributeOption::observe(AttributeOptionObserver::class);
+        Deposit::observe(DepositObserver::class);
         ItemProduct::observe(ItemProductObserver::class);
         Page::observe(PageObserver::class);
+        Payment::observe(PaymentObserver::class);
         Schema::observe(SchemaObserver::class);
     }
 }
