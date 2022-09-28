d08d4c35330054c32f4e84be46db40d632c264cc
Jędrzej Buliński <git@jedrula.pl>
2022-09-28 12:38:30 +0200

    New order mail

---
+++ resources/lang/pl/mail.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    // new order
+    'thanks' => 'Dziękujemy za zakupy w :name',
+    'number' => 'Twoje zamówienie ma numer :number, a jego szczegóły możesz sprawdzić poniżej.',
+    'info' => 'Informacje o zamówieniu',
+    'number-date' => 'Zamówienie numer :number z dnia :date',
+    'delivary' => 'Dostawa',
+    'products' => 'Zamówione produkty',
+    'name' => 'Nazwa',
+    'quantity' => 'Ilość',
+    'price' => 'Cena',
+    'summary-products' => 'Wartość produktów',
+    'summary-delivery' => 'Koszt dostawy',
+    'summary-discount' => 'Rabat',
+    'summary-paid' => 'Do zapłaty',
+    'purchaser' => 'Zamawiający',
+    'company' => 'Firma',
+    'name-surname' => 'Imię i nazwisko',
+    'address' => 'Adres',
+    'zip' => 'Kod pocztowy',
+    'city' => 'Miejscowość',
+    'phone' => 'Telefon kontaktowy',
+    'regards' => 'Pozdrawiamy',
+    'team' => 'zespół :team',
+];
