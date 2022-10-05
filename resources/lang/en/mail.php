cb17311b36b655cdfdb94ed1beada627ea356c33
Bartosz Juraszewski <shadow38pl@gmail.com>
2022-10-05 07:04:18 +0000

    Apply 10 suggestion(s) to 1 file(s)

--- resources/lang/en/mail.php
+++ resources/lang/en/mail.php
@@ -3,7 +3,7 @@
 return [
     // new order
     'subject-new-order' => 'Order :number confirmation',
-    'thanks' => 'Thanks you for order from :name',
+    'thanks' => 'Thanks you for ordering from :name',
     'number' => 'Your order number is :number.',
     'info' => 'Order',
     'number-date' => 'Order no. :number from :date',
@@ -23,24 +23,24 @@ return [
     'zip' => 'Zip code',
     'city' => 'City',
     'phone' => 'Phone',
-    'regards' => 'Regards',
+    'regards' => 'Best Regards',
     'team' => ':team crew',
 
     // status changed
-    'subject-status-changed' => 'Order :number status changed',
-    'status-changed' => 'Status of your order was changed.',
-    'status' => 'New status is',
+    'subject-status-changed' => 'Order :number status has changed',
+    'status-changed' => 'Status of your order has changed.',
+    'status' => 'The new status is',
 
     // password reset
     'hello' => 'Hello',
-    'reset-request' => 'We received request to change your password.',
-    'link' => 'To change your password click this link',
+    'reset-request' => 'We have received a request to change your password.',
+    'link' => 'Click this link to change your password',
     'change-password' => 'change password',
-    'warning' => 'If you not request for password change, pleas ignore this email',
+    'warning' => 'If you haven't requested the change of password, please ignore this email',
 
     // user registered
-    'register-thanks' => 'Thank you for register in :name',
-    'benefits' => 'By logging into your account, you will gain access to your personal customer panel,
-        where you will be able to see all your orders, along with information on their status.
+    'register-thanks' => 'Thank you for registering to :name',
+    'benefits' => 'By logging into your account, you will gain access to your personal customer panel.
+        You will be able to see all of your orders, along with information on their status.
         The customer account also allows you to manage your contact information and delivery addresses.',
 ];
