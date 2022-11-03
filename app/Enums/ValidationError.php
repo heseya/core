5fc0b6aba3cbc12fbdf5bb54ca1d2760e67bb9f6
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2022-11-03 12:13:22 +0100

    Added media slug validation key

--- app/Enums/ValidationError.php
+++ app/Enums/ValidationError.php
@@ -57,4 +57,5 @@ final class ValidationError extends Enum
     public const REQUIREDWITHALL = 'VALIDATION_REQUIRED_WITH_ALL';
     public const GTE = 'VALIDATION_GTE';
     public const REQUIREDCONSENTSUPDATE = 'VALIDATION_REQUIRED_CONSENTS';
+    public const MEDIASLUG = 'VALIDATION_MEDIA_SLUG';
 }
