418d4e405638de6e1de6410a3c0a0dfce09d9471
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2023-01-02 13:28:44 +0100

    Fix phpstan

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -95,7 +95,7 @@ class UserService implements UserServiceContract
                 ->get();
 
             $newRoles = $roleModels->diff($user->roles);
-            /** @var Collection<int, Role> $removedRoles */
+            /** @var \Illuminate\Database\Eloquent\Collection $removedRoles */
             $removedRoles = $user->roles->diff($roleModels);
 
             // @phpstan-ignore-next-line
@@ -107,7 +107,6 @@ class UserService implements UserServiceContract
                 throw new ClientException(Exceptions::CLIENT_GIVE_ROLE_THAT_USER_DOESNT_HAVE);
             }
 
-            // @phpstan-ignore-next-line
             $permissions = $removedRoles->flatMap(
                 fn (Role $role) => $role->type->value !== RoleType::AUTHENTICATED ? $role->getPermissionNames() : [],
             )->unique();
