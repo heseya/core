f6239cf68b5e482b9c60508a8acbc50d13fd768b
Jędrzej Buliński <git@jedrula.pl>
2023-01-17 10:56:28 +0100

    testNotAvailablePaymentMethod fix

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,7 +12,6 @@ use App\Http\Requests\PaymentUpdateRequest;
 use App\Http\Resources\PaymentResource;
 use App\Models\Order;
 use App\Models\Payment;
-use App\Models\PaymentMethod;
 use App\Payments\PayPal;
 use App\Payments\PayU;
 use App\Payments\Przelewy24;
@@ -20,12 +19,12 @@ use App\Services\Contracts\PaymentServiceContract;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Config;
-use Throwable;
 
 class PaymentController extends Controller
 {
-    public function __construct(private PaymentServiceContract $paymentService)
-    {
+    public function __construct(
+        private PaymentServiceContract $paymentService,
+    ) {
     }
 
     public function pay(Order $order, string $method, PayRequest $request): JsonResource
@@ -76,58 +75,14 @@ class PaymentController extends Controller
         return PaymentResource::make($payment);
     }
 
-//    public function store(PaymentStoreRequest $request): JsonResource
-//    {
-//        /** @var PaymentDto $dto */
-//        $dto = PaymentDto::instantiateFromRequest($request);
-//
-//        return PaymentResource::make(Payment::create($dto->toArray()));
-//    }
-
-    /**
-     * @throws ClientException
-     */
-    public function store(Order $order, string $method, PaymentStoreRequest $request): JsonResource
+    public function store(PaymentStoreRequest $request): JsonResource
     {
-        if ($order->paid) {
-            throw new ClientException(Exceptions::CLIENT_ORDER_PAID);
-        }
+        /** @var PaymentDto $dto */
+        $dto = PaymentDto::instantiateFromRequest($request);
 
-        if (!array_key_exists($method, Config::get('payable.aliases'))) {
-            throw new ClientException(Exceptions::CLIENT_UNKNOWN_PAYMENT_METHOD);
-        }
-
-        /**
-         * @var PayU|PayPal|Przelewy24 $methodClass
-         */
-        $methodClass = Config::get('payable.aliases')[$method];
-
-        $paymentMethod = PaymentMethod::searchByCriteria([
-            'order_code' => $order->code,
-            'alias' => $method,
-        ])->first();
-
-        if (!$paymentMethod) {
-            throw new ClientException(Exceptions::PAYMENT_METHOD_NOT_AVAILABLE_FOR_SHIPPING);
-        }
-
-        /**
-         * @var Payment $payment
-         */
-        $payment = $order->payments()->create([
-            'method' => $method,
-            'amount' => $order->summary - $order->paid_amount,
-            'paid' => false,
-            'continue_url' => $request->input('continue_url'),
-        ]);
-
-        try {
-            $payment->update($methodClass::generateUrl($payment));
-        } catch (Throwable) {
-            throw new ClientException(Exceptions::CLIENT_GENERATE_PAYMENT_URL);
-        }
-
-        return PaymentResource::make($payment);
+        return PaymentResource::make(
+            Payment::query()->create($dto->toArray()),
+        );
     }
 
     public function updatePayment(PaymentUpdateRequest $request, Payment $payment): JsonResource
--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -3,12 +3,14 @@
 namespace App\Services;
 
 use App\Enums\ExceptionsEnums\Exceptions;
-use App\Enums\PaymentStatus;
 use App\Exceptions\ClientException;
 use App\Exceptions\ServerException;
 use App\Models\Order;
 use App\Models\Payment;
 use App\Models\PaymentMethod;
+use App\Payments\PayPal;
+use App\Payments\PayU;
+use App\Payments\Przelewy24;
 use App\Services\Contracts\PaymentServiceContract;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
@@ -22,10 +24,11 @@ class PaymentService implements PaymentServiceContract
      */
     public function getPayment(Order $order, string $method, Request $request): Payment
     {
-        $paymentMethod = PaymentMethod::where('id', $method)->first();
-        return $paymentMethod === null ?
-            $this->createPayment($method, $order, $request)
-            : $this->requestPaymentFromMicroservice($paymentMethod, $order, $request);
+        $paymentMethod = PaymentMethod::query()->where('id', $method)->first();
+
+        return $paymentMethod instanceof PaymentMethod
+            ? $this->requestPaymentFromMicroservice($paymentMethod, $order, $request)
+            : $this->createPaymentLegacy($method, $order, $request);
     }
 
     private function requestPaymentFromMicroservice(PaymentMethod $method, Order $order, Request $request): Payment
@@ -52,25 +55,43 @@ class PaymentService implements PaymentServiceContract
         );
     }
 
-    private function createPayment(string $method, Order $order, Request $request): Payment
+    private function createPaymentLegacy(string $method, Order $order, Request $request): Payment
     {
+        if ($order->paid) {
+            throw new ClientException(Exceptions::CLIENT_ORDER_PAID);
+        }
+
         if (!array_key_exists($method, Config::get('payable.aliases'))) {
             throw new ClientException(Exceptions::CLIENT_UNKNOWN_PAYMENT_METHOD);
         }
 
-        $method_class = Config::get('payable.aliases')[$method];
+        /**
+         * @var PayU|PayPal|Przelewy24 $methodClass
+         */
+        $methodClass = Config::get('payable.aliases')[$method];
 
-        /** @var Payment $payment */
+        $paymentMethod = PaymentMethod::searchByCriteria([
+            'order_code' => $order->code,
+            'alias' => $method,
+        ])->first();
+
+        if (!$paymentMethod) {
+            throw new ClientException(Exceptions::PAYMENT_METHOD_NOT_AVAILABLE_FOR_SHIPPING);
+        }
+
+        /**
+         * @var Payment $payment
+         */
         $payment = $order->payments()->create([
             'method' => $method,
             'amount' => $order->summary - $order->paid_amount,
-            'status' => PaymentStatus::PENDING,
+            'paid' => false,
             'continue_url' => $request->input('continue_url'),
         ]);
 
         try {
-            $payment->update($method_class::generateUrl($payment));
-        } catch (Throwable $error) {
+            $payment->update($methodClass::generateUrl($payment));
+        } catch (Throwable) {
             throw new ClientException(Exceptions::CLIENT_GENERATE_PAYMENT_URL);
         }
 
