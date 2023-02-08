bdddd174864dd70888d9f45ec9369d4ad0b92019
Bartosz Juraszewski <shadow38pl@gmail.com>
2023-02-08 13:38:20 +0100

    Fix payment rounding error

--- app/Payments/PayU.php
+++ app/Payments/PayU.php
@@ -27,7 +27,7 @@ class PayU implements PaymentMethod
                 $client_id . '&client_secret=' . $client_secret,
         )->throw();
 
-        $amount = (int) ($payment->amount * 100);
+        $amount = round($payment->amount * 100, 0);
 
         $response = Http::withToken($response['access_token'])->withOptions([
             'allow_redirects' => false,
