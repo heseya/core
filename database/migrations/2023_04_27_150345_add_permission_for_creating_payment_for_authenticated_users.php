c5e216217c7896f9dc7ca491cabfbee062318a0b
Jędrzej Buliński <git@jedrula.pl>
2023-04-27 17:09:06 +0200

    🐛 Fixed payment creation by default user

---
+++ database/migrations/2023_04_27_150345_add_permission_for_creating_payment_for_authenticated_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Enums\RoleType;
+use App\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /** @var Role $authenticated */
+        $authenticated = Role::query()->where('type', RoleType::AUTHENTICATED)->firstOrFail();
+        $authenticated->givePermissionTo('payments.add');
+        $authenticated->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        /** @var Role $authenticated */
+        $authenticated = Role::query()->where('type', RoleType::AUTHENTICATED)->firstOrFail();
+        $authenticated->revokePermissionTo('payments.add');
+        $authenticated->save();
+    }
+};
