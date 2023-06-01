fb840abc25cd1577c84440d68d9e5a51fcfa8327
Jędrzej Buliński <git@jedrula.pl>
2023-06-01 23:26:22 +0200

    🦺 Better address validation

--- app/Rules/ShippingPlaceValidation.php
+++ app/Rules/ShippingPlaceValidation.php
@@ -2,60 +2,87 @@
 
 namespace App\Rules;
 
+use App\Enums\ExceptionsEnums\Exceptions;
 use App\Enums\ShippingType;
+use App\Exceptions\ServerException;
 use App\Models\Address;
 use App\Models\ShippingMethod;
+use Closure;
 use Illuminate\Contracts\Validation\DataAwareRule;
-use Illuminate\Contracts\Validation\ImplicitRule;
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Support\Facades\Validator;
 
-class ShippingPlaceValidation implements ImplicitRule, DataAwareRule
+final class ShippingPlaceValidation implements ValidationRule, DataAwareRule
 {
-    protected array $data = [];
-
     /**
-     * Determine if the validation rule passes.
+     * All the data under validation.
      *
-     * @param string $attribute
+     * @var array<string, mixed>
      */
-    public function passes($attribute, mixed $value): bool
-    {
-        if (!array_key_exists('shipping_method_id', $this->data) || $this->data['shipping_method_id'] === null) {
-            return true;
-        }
-
-        /** @var ShippingMethod|null $shippingMethod */
-        $shippingMethod = ShippingMethod::query()->find($this->data['shipping_method_id']);
-
-        return match ($shippingMethod?->shipping_type) {
-            ShippingType::POINT => Address::query()->where('id', $value)->exists(),
-            ShippingType::ADDRESS => !Validator::make($this->data, [
-                'shipping_place' => ['nullable', 'array', new ShippingAddressRequired()],
-                'shipping_place.name' => ['string', 'max:255'],
-                'shipping_place.phone' => ['string', 'max:20'],
-                'shipping_place.address' => ['string', 'max:255'],
-                'shipping_place.zip' => ['string', 'max:16'],
-                'shipping_place.city' => ['string', 'max:255'],
-                'shipping_place.country' => ['string', 'size:2'],
-                'shipping_place.vat' => ['nullable', 'string', 'max:15'],
-            ])->fails(),
-            ShippingType::POINT_EXTERNAL => is_string($value),
-            default => true,
-        };
-    }
+    private array $data = [];
 
     /**
-     * Get the validation error message.
+     * Set the data under validation.
+     *
+     * @param array<string, mixed> $data
      */
-    public function message(): string
-    {
-        return 'Shipping place data is incorrect.';
-    }
-
-    public function setData($data): ShippingPlaceValidation
+    public function setData(array $data): self
     {
         $this->data = $data;
 
         return $this;
+    }
+
+    /**
+     * @throws ServerException
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!array_key_exists('shipping_method_id', $this->data) || $this->data['shipping_method_id'] === null) {
+            return;
+        }
+
+        /** @var ShippingMethod $shippingMethod */
+        $shippingMethod = ShippingMethod::query()->findOr(
+            $this->data['shipping_method_id'],
+            fn () => $fail('Shipping method does not exist.'),
+        );
+
+        match ($shippingMethod->shipping_type) {
+            ShippingType::ADDRESS => $this->address($fail),
+            ShippingType::POINT => $this->point($value, $fail),
+            ShippingType::POINT_EXTERNAL => $this->pointExternal($value, $fail),
+            default => throw new ServerException(Exceptions::SERVER_SHIPPING_TYPE_NO_VALIDATION),
+        };
+    }
+
+    private function point(mixed $value, Closure $fail): void
+    {
+        if (Address::query()->where('id', $value)->doesntExist()) {
+            $fail('Shipping point does not exist.');
+        }
+    }
+
+    private function pointExternal(mixed $value, Closure $fail): void
+    {
+        if (!is_string($value)) {
+            $fail('Shipping point should be string.');
+        }
+    }
+
+    private function address(Closure $fail): void
+    {
+        if (Validator::make($this->data, [
+            'shipping_place' => ['nullable', 'array', new ShippingAddressRequired()],
+            'shipping_place.name' => ['string', 'max:255'],
+            'shipping_place.phone' => ['string', 'max:20'],
+            'shipping_place.address' => ['string', 'max:255'],
+            'shipping_place.zip' => ['string', 'max:16'],
+            'shipping_place.city' => ['string', 'max:255'],
+            'shipping_place.country' => ['string', 'size:2'],
+            'shipping_place.vat' => ['nullable', 'string', 'max:15'],
+        ])->fails()) {
+            $fail('Shipping address in invalid.');
+        }
     }
 }
