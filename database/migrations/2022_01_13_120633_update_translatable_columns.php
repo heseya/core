c69d10dfcc85e2a1e46097f74d43c7531057c464
Jędrzej Buliński <git@jedrula.pl>
2023-08-22 16:31:15 +0200

    migrations fix

--- database/migrations/2022_01_13_120633_update_translatable_columns.php
+++ database/migrations/2022_01_13_120633_update_translatable_columns.php
@@ -6,10 +6,12 @@ use App\Models\Schema;
 use App\Models\Status;
 use Domain\Language\Language;
 use Domain\Page\Page;
+use Domain\ProductSet\ProductSet;
 use Domain\Seo\Models\SeoMetadata;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema as DbSchema;
+use Support\Utils\Migrate;
 
 class UpdateTranslatableColumns extends Migration
 {
@@ -18,8 +20,7 @@ class UpdateTranslatableColumns extends Migration
      */
     public function up(): void
     {
-        $language = Language::query()->where('default', true)->firstOrFail();
-        $lang = $language->getKey();
+        $lang = Language::query()->where('default', true)->value('id');
 
         DbSchema::table('products', function (Blueprint $table): void {
             $table->text('name')->change();
@@ -28,17 +29,12 @@ class UpdateTranslatableColumns extends Migration
             $table->text('published')->nullable();
         });
 
-        Product::chunk(100, fn ($products) => $products->each(
-            function (Product $product) use ($lang): void {
-                $attr = $product->getAttributes();
-                $product
-                    ->setAttribute('published', [$lang])
-                    ->setTranslation('name', $lang, $attr['name'])
-                    ->setTranslation('description_html', $lang, $attr['description_html'])
-                    ->setTranslation('description_short', $lang, $attr['description_short'])
-                    ->save();
-            },
-        ));
+        Product::query()->update([
+            'name' => Migrate::lang('name', $lang),
+            'description_html' => Migrate::lang('description_html', $lang),
+            'description_short' => Migrate::lang('description_short', $lang),
+            'published' => [$lang],
+        ]);
 
         DbSchema::table('schemas', function (Blueprint $table): void {
             $table->text('name')->change();
@@ -46,29 +42,19 @@ class UpdateTranslatableColumns extends Migration
             $table->text('published')->nullable();
         });
 
-        Schema::chunk(100, fn ($schemas) => $schemas->each(
-            function (Schema $schema) use ($lang): void {
-                $attr = $schema->getAttributes();
-                $schema
-                    ->setAttribute('published', [$lang])
-                    ->setTranslation('name', $lang, $attr['name'])
-                    ->setTranslation('description', $lang, $attr['description'])
-                    ->save();
-            },
-        ));
+        Schema::query()->update([
+            'name' => Migrate::lang('name', $lang),
+            'description' => Migrate::lang('description', $lang),
+            'published' => [$lang],
+        ]);
 
         DbSchema::table('options', function (Blueprint $table): void {
             $table->text('name')->change();
         });
 
-        Option::chunk(100, fn ($options) => $options->each(
-            function (Option $option) use ($lang): void {
-                $attr = $option->getAttributes();
-                $option
-                    ->setTranslation('name', $lang, $attr['name'])
-                    ->save();
-            },
-        ));
+        Option::query()->update([
+            'name' => Migrate::lang('name', $lang),
+        ]);
 
         DbSchema::table('pages', function (Blueprint $table): void {
             $table->text('name')->change();
@@ -76,16 +62,11 @@ class UpdateTranslatableColumns extends Migration
             $table->text('published')->nullable();
         });
 
-        Page::chunk(100, fn ($pages) => $pages->each(
-            function (Page $page) use ($lang): void {
-                $attr = $page->getAttributes();
-                $page
-                    ->setAttribute('published', [$lang])
-                    ->setTranslation('name', $lang, $attr['name'])
-                    ->setTranslation('content_html', $lang, $attr['content_html'])
-                    ->save();
-            },
-        ));
+        Page::query()->update([
+            'name' => Migrate::lang('name', $lang),
+            'content_html' => Migrate::lang('content_html', $lang),
+            'published' => [$lang],
+        ]);
 
         DbSchema::table('statuses', function (Blueprint $table): void {
             $table->text('name')->change();
@@ -93,16 +74,11 @@ class UpdateTranslatableColumns extends Migration
             $table->text('published')->nullable();
         });
 
-        Status::chunk(100, fn ($statuses) => $statuses->each(
-            function (Status $status) use ($lang): void {
-                $attr = $status->getAttributes();
-                $status
-                    ->setAttribute('published', [$lang])
-                    ->setTranslation('name', $lang, $attr['name'])
-                    ->setTranslation('description', $lang, $attr['description'])
-                    ->save();
-            },
-        ));
+        Status::query()->update([
+            'name' => Migrate::lang('name', $lang),
+            'description' => Migrate::lang('description', $lang),
+            'published' => [$lang],
+        ]);
 
         DbSchema::table('seo_metadata', function (Blueprint $table): void {
             $table->text('title')->nullable()->change();
@@ -112,10 +88,9 @@ class UpdateTranslatableColumns extends Migration
             $table->text('published')->nullable();
         });
 
-        SeoMetadata::chunk(100, fn ($seo) => $seo->each(
+        SeoMetadata::query()->chunk(100, fn ($seo) => $seo->each(
             function (SeoMetadata $seo) use ($lang): void {
                 $attr = $seo->getAttributes();
-
                 $seo
                     ->setRawAttributes([
                         'no_index' => json_encode([$lang => (bool) $attr['no_index']]),
@@ -134,16 +109,11 @@ class UpdateTranslatableColumns extends Migration
             $table->text('published')->nullable();
         });
 
-        Status::chunk(100, fn ($statuses) => $statuses->each(
-            function (Status $status) use ($lang): void {
-                $attr = $status->getAttributes();
-                $status
-                    ->setAttribute('published', [$lang])
-                    ->setTranslation('name', $lang, $attr['name'])
-                    ->setTranslation('description', $lang, $attr['description'])
-                    ->save();
-            },
-        ));
+        ProductSet::query()->update([
+            'name' => Migrate::lang('name', $lang),
+            'description_html' => Migrate::lang('description_html', $lang),
+            'published' => [$lang],
+        ]);
     }
 
     /**
--- database/migrations/2023_08_10_085422_update_translatable_columns_in_banner_media.php
+++ database/migrations/2023_08_10_085422_update_translatable_columns_in_banner_media.php
@@ -1,8 +1,11 @@
 <?php
 
+use Domain\Banner\Models\BannerMedia;
+use Domain\Language\Language;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Support\Utils\Migrate;
 
 return new class extends Migration {
     public function up(): void
@@ -12,6 +15,14 @@ return new class extends Migration {
             $table->text('subtitle')->change();
             $table->text('published')->nullable();
         });
+
+        $lang = Language::query()->where('default', true)->value('id');
+
+        BannerMedia::query()->update([
+            'title' => Migrate::lang('title', $lang),
+            'subtitle' => Migrate::lang('subtitle', $lang),
+            'published' => [$lang],
+        ]);
     }
 
     public function down(): void
