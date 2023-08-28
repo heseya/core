01c576674d24ae7c50fd5c62943c2c261122fb74
Jędrzej Buliński <git@jedrula.pl>
2023-08-28 10:44:12 +0200

    HES-2234

--- app/Http/Resources/ItemPublicResource.php
+++ app/Http/Resources/ItemPublicResource.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
+/**
+ * @property Item $resource
+ */
 class ItemPublicResource extends Resource
 {
+    /**
+     * @return array<string, mixed>
+     */
     public function base(Request $request): array
     {
         return [
             'id' => $this->resource->getKey(),
             'name' => $this->resource->name,
             'sku' => $this->resource->sku,
+            'quantity' => $this->resource->quantity_real,
         ];
     }
 }
