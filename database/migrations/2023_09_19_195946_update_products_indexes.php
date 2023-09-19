25e52d9beb3960babad6d59cf3ffabebfd7a2113
Jędrzej Buliński <git@jedrula.pl>
2023-09-19 22:01:46 +0200

    fix migrations

--- database/migrations/2023_09_19_195946_update_products_indexes.php
+++ database/migrations/2023_09_19_195946_update_products_indexes.php
@@ -8,16 +8,19 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table): void {
-            $table->text('search_values')->nullable();
-            $table->fullText('search_values');
+            $table->dropFullText('products_search_values_fulltext');
+            $table->fullText(
+                ['search_values', 'slug', 'name', 'description_short', 'description_html'],
+                'products_fulltext',
+            );
         });
     }
 
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table): void {
-            $table->dropFullText('products_search_values_fulltext');
-            $table->dropColumn('search_values');
+            $table->dropFullText('products_fulltext');
+            $table->fullText('search_values');
         });
     }
 };
