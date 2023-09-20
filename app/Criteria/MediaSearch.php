c8bce4003c645b7fbb9a4b69597fc0f1dc1dc219
Dawid Miklas <dawid.miklas@escolasoft.com>
2023-09-20 08:19:34 +0200

    fixed media search

--- app/Criteria/MediaSearch.php
+++ app/Criteria/MediaSearch.php
@@ -10,10 +10,13 @@ class MediaSearch extends Criterion
     public function query(Builder $query): Builder
     {
         if ($this->value) {
-            $query->where('url', 'like', "%{$this->value}%")
-                ->orWhere('slug', 'like', "%{$this->value}%")
-                ->orWhere('alt', 'like', "%{$this->value}%")
-                ->orWhere('type', 'like', "%{$this->value}%");
+            $query->where(function (Builder $query): void {
+                $query
+                    ->where('url', 'like', "%{$this->value}%")
+                    ->orWhere('slug', 'like', "%{$this->value}%")
+                    ->orWhere('alt', 'like', "%{$this->value}%")
+                    ->orWhere('type', 'like', "%{$this->value}%");
+            });
         }
 
         return $query;
