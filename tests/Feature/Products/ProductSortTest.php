cce95794914004215c9a9000c5036e740b6a76bd
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2023-09-26 09:11:44 +0200

    Fix sort products by attribute when using slug

--- tests/Feature/Products/ProductSortTest.php
+++ tests/Feature/Products/ProductSortTest.php
@@ -12,7 +12,7 @@ class ProductSortTest extends TestCase
     /**
      * @dataProvider authProvider
      */
-    public function testSortByAttribute(string $user): void
+    public function testSortByAttributeWithId(string $user): void
     {
         $this->{$user}->givePermissionTo('products.show');
 
@@ -32,6 +32,51 @@ class ProductSortTest extends TestCase
         $this->assertEquals($product1->getKey(), $response->json('data.0.id'));
         $this->assertEquals($product2->getKey(), $response->json('data.1.id'));
         $this->assertEquals($product3->getKey(), $response->json('data.2.id'));
+
+        $response = $this
+            ->actingAs($this->{$user})
+            ->json('GET', '/products', [
+                'sort' => "attribute.{$attribute->getKey()}:asc",
+            ]);
+
+        $this->assertEquals($product3->getKey(), $response->json('data.0.id'));
+        $this->assertEquals($product2->getKey(), $response->json('data.1.id'));
+        $this->assertEquals($product1->getKey(), $response->json('data.2.id'));
+    }
+
+    /**
+     * @dataProvider authProvider
+     */
+    public function testSortByAttributeWithSlug(string $user): void
+    {
+        $this->{$user}->givePermissionTo('products.show');
+
+        /** @var Attribute $attribute */
+        $attribute = Attribute::factory()->create();
+
+        $product1 = $this->createProductWithAttribute($attribute, '2023-12-12');
+        $product2 = $this->createProductWithAttribute($attribute, '2023-05-05');
+        $product3 = $this->createProductWithAttribute($attribute, '2023-01-02');
+
+        $response = $this
+            ->actingAs($this->{$user})
+            ->json('GET', '/products', [
+                'sort' => "attribute.{$attribute->slug}:desc",
+            ]);
+
+        $this->assertEquals($product1->getKey(), $response->json('data.0.id'));
+        $this->assertEquals($product2->getKey(), $response->json('data.1.id'));
+        $this->assertEquals($product3->getKey(), $response->json('data.2.id'));
+
+        $response = $this
+            ->actingAs($this->{$user})
+            ->json('GET', '/products', [
+                'sort' => "attribute.{$attribute->slug}:asc",
+            ]);
+
+        $this->assertEquals($product3->getKey(), $response->json('data.0.id'));
+        $this->assertEquals($product2->getKey(), $response->json('data.1.id'));
+        $this->assertEquals($product1->getKey(), $response->json('data.2.id'));
     }
 
     private function createProductWithAttribute(Attribute $attribute, string $optionName): Product
