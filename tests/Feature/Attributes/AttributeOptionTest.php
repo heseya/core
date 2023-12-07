28fa37441b49ccb6e1b48191f994ca625491a5af
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2023-12-07 14:48:46 +0100

    Added product_set_slug to filter attribute options

--- tests/Feature/Attributes/AttributeOptionTest.php
+++ tests/Feature/Attributes/AttributeOptionTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Attributes;
 
+use App\Models\Product;
 use Domain\ProductAttribute\Models\Attribute;
 use Domain\ProductAttribute\Models\AttributeOption;
+use Domain\ProductSet\ProductSet;
 use Tests\TestCase;
 
 class AttributeOptionTest extends TestCase
@@ -85,5 +87,117 @@ class AttributeOptionTest extends TestCase
                 'search' => null,
             ])
             ->assertUnprocessable();
+    }
+
+    /**
+     * @dataProvider authProvider
+     */
+    public function testSearchByProductSetSlug($user): void
+    {
+        $this->{$user}->givePermissionTo('attributes.show');
+
+        $parent = ProductSet::factory()->create([
+            'public' => true,
+            'slug' => 'parent',
+        ]);
+        $child = ProductSet::factory()->create([
+            'public' => true,
+            'parent_id' => $parent->getKey(),
+            'slug' => 'child',
+        ]);
+        $subChild = ProductSet::factory()->create([
+            'public' => true,
+            'parent_id' => $child->getKey(),
+            'slug' => 'subChild',
+        ]);
+
+        $set = ProductSet::factory()->create([
+            'public' => true,
+            'slug' => 'set',
+        ]);
+
+        $attribute = Attribute::factory()->create();
+        $target = AttributeOption::factory()->create([
+            'name' => 'searchtarget',
+            'attribute_id' => $attribute->getKey(),
+            'index' => 0,
+        ]);
+
+        /** @var AttributeOption $optionParent */
+        $optionParent = AttributeOption::factory()->create([
+            'name' => 'Parent',
+            'attribute_id' => $attribute->getKey(),
+            'index' => 0,
+        ]);
+        /** @var AttributeOption $optionChild */
+        $optionChild = AttributeOption::factory()->create([
+            'name' => 'Child',
+            'attribute_id' => $attribute->getKey(),
+            'index' => 1,
+        ]);
+        /** @var AttributeOption $optionSubChild */
+        $optionSubChild = AttributeOption::factory()->create([
+            'name' => 'SubChild',
+            'attribute_id' => $attribute->getKey(),
+            'index' => 2,
+        ]);
+        /** @var AttributeOption $optionSet */
+        $optionSet = AttributeOption::factory()->create([
+            'name' => 'Set',
+            'attribute_id' => $attribute->getKey(),
+            'index' => 3,
+        ]);
+
+        $productParent = Product::factory()->create([
+            'public' => true,
+        ]);
+        $productParent->sets()->sync([$parent->getKey()]);
+        $productParent->attributes()->attach($attribute->getKey());
+        $productParent->attributes->first()->product_attribute_pivot->options()->attach($optionParent->getKey());
+
+        $productChild = Product::factory()->create([
+            'public' => true,
+        ]);
+        $productChild->sets()->sync([$child->getKey()]);
+        $productChild->attributes()->attach($attribute->getKey());
+        $productChild->attributes->first()->product_attribute_pivot->options()->attach($optionChild->getKey());
+
+        $productSubChild = Product::factory()->create([
+            'public' => true,
+        ]);
+        $productSubChild->sets()->sync([$subChild->getKey()]);
+        $productSubChild->attributes()->attach($attribute->getKey());
+        $productSubChild->attributes->first()->product_attribute_pivot->options()->attach($optionSubChild->getKey());
+
+        $product = Product::factory()->create([
+            'public' => true,
+        ]);
+        $product->sets()->sync([$set->getKey()]);
+        $product->attributes()->attach($attribute->getKey());
+        $product->attributes->first()->product_attribute_pivot->options()->attach($optionSet->getKey());
+
+        $this
+            ->actingAs($this->{$user})
+            ->json('GET', "/attributes/id:{$attribute->getKey()}/options", [
+                'product_set_slug' => $parent->slug,
+            ])
+            ->assertOk()
+            ->assertJsonCount(3, 'data')
+            ->assertJsonFragment(['name' => $optionParent->name])
+            ->assertJsonFragment(['name' => $optionChild->name])
+            ->assertJsonFragment(['name' => $optionSubChild->name])
+            ->assertJsonMissing(['name' => $optionSet->name]);
+
+        $this
+            ->actingAs($this->{$user})
+            ->json('GET', "/attributes/id:{$attribute->getKey()}/options", [
+                'product_set_slug' => $child->slug,
+            ])
+            ->assertOk()
+            ->assertJsonCount(2, 'data')
+            ->assertJsonFragment(['name' => $optionChild->name])
+            ->assertJsonFragment(['name' => $optionSubChild->name])
+            ->assertJsonMissing(['name' => $optionParent->name])
+            ->assertJsonMissing(['name' => $optionSet->name]);
     }
 }
