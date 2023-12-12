c8f0d42a4a8960e7702a0177333124d0eb7f4af3
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2023-12-12 10:48:04 +0100

    CR fix

--- database/migrations/2023_12_11_123302_update_order_buyer_relation.php
+++ database/migrations/2023_12_11_123302_update_order_buyer_relation.php
@@ -6,39 +6,22 @@ use Illuminate\Support\Facades\DB;
 return new class extends Migration {
     public function up(): void
     {
-        $this->updateTypeValue('orders', 'buyer_type', 'App\Models\User', 'User', 'buyer_type', 'buyer_id');
-        $this->updateTypeValue('orders', 'buyer_type', 'App\Models\App', 'App', 'buyer_type', 'buyer_id');
+        $this->updateTypeValue('orders', 'buyer_type', 'App\Models\User', 'User');
+        $this->updateTypeValue('orders', 'buyer_type', 'App\Models\App', 'App');
     }
 
     public function down(): void
     {
-        $this->updateTypeValue('orders', 'buyer_type', 'User', 'App\Models\User', 'buyer_type', 'buyer_id');
-        $this->updateTypeValue('orders', 'buyer_type', 'App', 'App\Models\App', 'buyer_type', 'buyer_id');
+        $this->updateTypeValue('orders', 'buyer_type', 'User', 'App\Models\User');
+        $this->updateTypeValue('orders', 'buyer_type', 'App', 'App\Models\App');
     }
 
     private function updateTypeValue(
         string $table,
-        string $typeColumn,
+        string $column,
         string $oldValue,
         string $newValue,
-        string $idColumn,
-        string $idSecondColumn = null,
     ): void {
-        DB::table($table)
-            ->orderBy($idColumn)
-            ->chunk(100, function ($records) use ($table, $typeColumn, $newValue, $idColumn, $idSecondColumn, $oldValue) {
-                foreach ($records as $record) {
-                    if ($record->$typeColumn === $oldValue) {
-                        $query = DB::table($table)
-                            ->where($idColumn, '=', $record->$idColumn);
-                        if ($idSecondColumn) {
-                            $query->where($idSecondColumn, '=', $record->$idSecondColumn);
-                        }
-                        $query->update([
-                            $typeColumn => $newValue,
-                        ]);
-                    }
-                }
-            });
+        DB::table($table)->where($column, '=', $oldValue)->update([$column => $newValue]);
     }
 };
