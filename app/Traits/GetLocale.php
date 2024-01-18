a7047485f80ef6c72f941328d7c9df9b7a85e2da
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2024-01-18 15:13:05 +0100

    Auth mails with translations

--- app/Traits/GetLocale.php
+++ app/Traits/GetLocale.php
@@ -2,25 +2,26 @@
 
 namespace App\Traits;
 
-use Domain\SalesChannel\Models\SalesChannel;
+use Domain\Language\LanguageService;
 use Domain\SalesChannel\SalesChannelRepository;
+use Illuminate\Support\Facades\App;
 
 trait GetLocale
 {
     public function getLocaleFromRequest(): string
     {
-        /** @var string|null $locale */
-        $locale = request()->header('Content-Language');
-
-        if (!$locale) {
-            /** @var SalesChannel $salesChannel */
-            $salesChannel = request()->header('X-Sales-Channel')
-                ? app(SalesChannelRepository::class)->getOne(request()->header('X-Sales-Channel'))
-                : app(SalesChannelRepository::class)->getDefault();
-            /** @var string $locale */
-            $locale = $salesChannel->defaultLanguage?->iso;
+        if (!request()->header('Accept-Language')) {
+            /** @var string|null $salesChannel */
+            $salesChannel = request()->header('X-Sales-Channel');
+            if ($salesChannel) {
+                /** @var string $locale */
+                $locale = app(SalesChannelRepository::class)->getOne($salesChannel)->defaultLanguage?->iso;
+                return $locale;
+            }
         }
 
+        /** @var string $locale */
+        $locale = app(LanguageService::class)->firstByIdOrDefault(App::getLocale())->iso;
         return $locale;
     }
 }
