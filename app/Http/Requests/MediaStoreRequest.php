892d25678c19a1bbc027f9e94821ba74cc2ba3f8
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2024-02-02 11:53:39 +0100

    Added log to media store

--- app/Http/Requests/MediaStoreRequest.php
+++ app/Http/Requests/MediaStoreRequest.php
@@ -5,6 +5,8 @@ namespace App\Http\Requests;
 use App\Enums\MediaSource;
 use App\Enums\MediaType;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules\Enum;
 
 class MediaStoreRequest extends FormRequest
@@ -14,6 +16,20 @@ class MediaStoreRequest extends FormRequest
         $images = 'image/jpeg,image/png,image/gif,image/bmp,image/svg+xml,image/webp,';
         $videos = 'video/mp4,video/webm,video/ogg,video/quicktime,video/x-ms-wmv,video/x-ms-asf,video/x-msvideo,';
         $documents = 'application/pdf';
+
+        /** @var UploadedFile $file */
+        $file = $this->file()['file'];
+        Log::info('----------------------------------------------');
+        Log::info('Media Store File name ' . $file->getFilename());
+        Log::info('Media Store File client name ' . $file->getClientOriginalName());
+        Log::info('Media Store File mime ' . $file->getMimeType());
+        Log::info('Media Store File client mime ' . $file->getClientMimeType());
+        Log::info('Media Store File size ' . $file->getSize());
+        Log::info('Media Store File real path ' . $file->getRealPath());
+        Log::info('Media Store File error ' . $file->getError());
+        Log::info('Media Store File error message ' . $file->getErrorMessage());
+        Log::info('Media Store File type ' . $file->getType());
+        Log::info('----------------------------------------------');
 
         return [
             'id' => ['uuid'],
