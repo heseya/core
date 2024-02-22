1a8a93167e04049e21e56588dd4e2554e44e0d43
Jędrzej Buliński <git@jedrula.pl>
2024-02-22 11:42:42 +0100

    address validation fix

--- app/Rules/StreetNumber.php
+++ app/Rules/StreetNumber.php
@@ -10,7 +10,7 @@ class StreetNumber implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!preg_match('/^[a-zA-Z0-9\p{L}\-\'.]+(?:\s[a-zA-Z1-9\p{L}\-\'.]+)*(?:\s[0-9]+[a-zA-Z]*(?:\/[0-9]+[a-zA-Z]*)*)+$/u', $value)) {
+        if (!preg_match('/\d/', $value)) {
             $fail(Exceptions::CLIENT_STREET_NUMBER->value);
         }
     }
