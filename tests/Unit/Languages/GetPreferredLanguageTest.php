38b3e6f417199fb915ce2f7a26efaf959ff6da43
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2024-03-11 07:19:14 +0100

    GetPreferredLanguageTest refactor

--- tests/Unit/Languages/GetPreferredLanguageTest.php
+++ tests/Unit/Languages/GetPreferredLanguageTest.php
@@ -11,153 +11,85 @@ class GetPreferredLanguageTest extends TestCase
 {
     use GetPreferredLanguage;
 
-    public function testDefaultNoHeader(): void
+    private Language $default;
+    private Language $fr;
+    private Language $de;
+    /** @var Collection<int, Language>  */
+    private Collection $languages;
+
+    public function setUp(): void
     {
-        $default = Language::make([
+        parent::setUp();
+
+        $this->default = Language::make([
             'iso' => 'pl',
             'default' => true,
         ]);
 
-        $fr = Language::make([
+        $this->fr = Language::make([
             'iso' => 'fr',
             'default' => false,
         ]);
 
-        $de = Language::make([
+        $this->de = Language::make([
             'iso' => 'de',
             'default' => false,
         ]);
 
-        $languages = Collection::make([$default, $fr, $de]);
+        $this->languages = Collection::make([$this->default, $this->fr, $this->de]);
+    }
 
+    public function testDefaultNoHeader(): void
+    {
         $this->assertEquals(
-            $default,
-            $this->getPreferredLanguage(null, $languages),
+            $this->default,
+            $this->getPreferredLanguage(null, $this->languages),
         );
     }
 
     public function testDefaultMissingLanguages(): void
     {
-        $default = Language::make([
-            'iso' => 'pl',
-            'default' => true,
-        ]);
-
-        $fr = Language::make([
-            'iso' => 'fr',
-            'default' => false,
-        ]);
-
-        $de = Language::make([
-            'iso' => 'de',
-            'default' => false,
-        ]);
-
-        $languages = Collection::make([$default, $fr, $de]);
-
         $this->assertEquals(
-            $default,
-            $this->getPreferredLanguage('en,en-GB;q=0.9,en-US;q=0.8', $languages),
+            $this->default,
+            $this->getPreferredLanguage('en,en-GB;q=0.9,en-US;q=0.8', $this->languages),
         );
     }
 
     public function testMatchingLanguage(): void
     {
-        $default = Language::make([
-            'iso' => 'pl',
-            'default' => true,
-        ]);
-
-        $fr = Language::make([
-            'iso' => 'fr',
-            'default' => false,
-        ]);
-
-        $de = Language::make([
-            'iso' => 'de',
-            'default' => false,
-        ]);
-
-        $languages = Collection::make([$default, $fr, $de]);
-
         $this->assertEquals(
-            $de,
-            $this->getPreferredLanguage('en,en-GB;q=0.9,en-US;q=0.8,de;q=0.7', $languages),
+            $this->de,
+            $this->getPreferredLanguage('en,en-GB;q=0.9,en-US;q=0.8,de;q=0.7', $this->languages),
         );
     }
 
     public function testMatchingMultipleLanguages(): void
     {
-        $default = Language::make([
-            'iso' => 'pl',
-            'default' => true,
-        ]);
-
-        $fr = Language::make([
-            'iso' => 'fr',
-            'default' => false,
-        ]);
-
-        $de = Language::make([
-            'iso' => 'de',
-            'default' => false,
-        ]);
-
-        $languages = Collection::make([$default, $fr, $de]);
-
         $this->assertEquals(
-            $fr,
-            $this->getPreferredLanguage('en,en-GB;q=0.9,fr;q=0.8,de;q=0.7', $languages),
+            $this->fr,
+            $this->getPreferredLanguage('en,en-GB;q=0.9,fr;q=0.8,de;q=0.7', $this->languages),
         );
     }
 
     public function testMatchingMultipleLanguagesSamePriority(): void
     {
-        $default = Language::make([
-            'iso' => 'pl',
-            'default' => true,
-        ]);
-
-        $fr = Language::make([
-            'iso' => 'fr',
-            'default' => false,
-        ]);
-
-        $de = Language::make([
-            'iso' => 'de',
-            'default' => false,
-        ]);
-
-        $languages = Collection::make([$default, $fr, $de]);
-
         $this->assertEquals(
-            $fr,
-            $this->getPreferredLanguage('en,en-GB;q=0.9,fr;q=0.8,de;q=0.8', $languages),
+            $this->fr,
+            $this->getPreferredLanguage('en,en-GB;q=0.9,fr;q=0.8,de;q=0.8', $this->languages),
         );
     }
 
     public function testMatchingRegionString(): void
     {
-        $default = Language::make([
-            'iso' => 'pl',
-            'default' => true,
-        ]);
-
         $en = Language::make([
             'iso' => 'en-US',
             'default' => false,
         ]);
-
-        $de = Language::make([
-            'iso' => 'de',
-            'default' => false,
-        ]);
-
-        $languages = Collection::make([$default, $en, $de]);
+        $this->languages = Collection::make([$this->default, $en, $this->de]);
 
         $this->assertEquals(
             $en,
-            $this->getPreferredLanguage('en', $languages),
+            $this->getPreferredLanguage('en', $this->languages),
         );
     }
 }
