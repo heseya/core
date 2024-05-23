41b037f2cc70a704e6134f8b35faf11490bda97e
Bartosz Juraszewski <bjuraszewski@heseya.com>
2024-05-23 11:51:43 +0200

    Added env flag

--- app/Rules/FullName.php
+++ app/Rules/FullName.php
@@ -5,12 +5,13 @@ namespace App\Rules;
 use App\Enums\ExceptionsEnums\Exceptions;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Config;
 
 class FullName implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!preg_match('/^[a-zA-Z\p{L}\-\']{2,}(?:\s[a-zA-Z\p{L}\-\']{2,})+$/u', $value)) {
+        if (Config::get('flags.validate_address_fullname') && !preg_match('/^[a-zA-Z\p{L}\-\']{2,}(?:\s[a-zA-Z\p{L}\-\']{2,})+$/u', $value)) {
             $fail(Exceptions::CLIENT_FULL_NAME->value);
         }
     }
