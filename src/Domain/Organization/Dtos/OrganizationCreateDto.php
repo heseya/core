fafbc0265555b10797230c43a718851f05ef77f3
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-07-18 09:54:26 +0200

    CR fixes

--- src/Domain/Organization/Dtos/OrganizationCreateDto.php
+++ src/Domain/Organization/Dtos/OrganizationCreateDto.php
@@ -9,12 +9,12 @@ use Domain\Address\Dtos\AddressCreateDto;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Attributes\Validation\Exists;
 use Spatie\LaravelData\Attributes\Validation\Min;
+use Spatie\LaravelData\Attributes\Validation\Rule;
 use Spatie\LaravelData\Attributes\Validation\Unique;
 use Spatie\LaravelData\Attributes\Validation\Uuid;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\DataCollection;
 use Spatie\LaravelData\Optional;
-use Spatie\LaravelData\Support\Validation\ValidationContext;
 
 final class OrganizationCreateDto extends Data
 {
@@ -24,6 +24,7 @@ final class OrganizationCreateDto extends Data
     public function __construct(
         #[Unique('organizations', 'client_id')]
         public readonly Optional|string|null $client_id,
+        #[Rule(new OrganizationUniqueVat())]
         public readonly string $billing_email,
         public readonly AddressCreateDto $billing_address,
         #[Uuid, Exists('sales_channels', 'id')]
@@ -31,14 +32,4 @@ final class OrganizationCreateDto extends Data
         #[DataCollectionOf(OrganizationSavedAddressCreateDto::class), Min(1)]
         public readonly DataCollection $shipping_addresses,
     ) {}
-
-    /**
-     * @return array<string, mixed>
-     */
-    public static function rules(ValidationContext $context): array
-    {
-        return [
-            'billing_address' => [new OrganizationUniqueVat()],
-        ];
-    }
 }
