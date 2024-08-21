399c3d7e077f2bdfec9d4ed11eff5731c6c788be
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-08-21 14:52:11 +0200

    Add email validation

--- src/Domain/Email/Dtos/EmailDto.php
+++ src/Domain/Email/Dtos/EmailDto.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Domain\Email\Dtos;
 
+use Spatie\LaravelData\Attributes\Validation\Email;
 use Spatie\LaravelData\Data;
 
 final class EmailDto extends Data
 {
     public function __construct(
         public readonly string $title,
+        #[Email]
         public readonly string $receiver,
         public readonly string $body,
     ) {}
