df865866b3efd461ca7eaa69e71dc2cda6b0501a
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2024-09-09 15:18:38 +0200

    Create prices for all missing pricemap - currency pairs

--- database/migrations/2024_09_06_063514_fix_missing_schema_options_prices.php
+++ database/migrations/2024_09_06_063514_fix_missing_schema_options_prices.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Models\Option;
-use Domain\PriceMap\PriceMap;
-use Domain\PriceMap\PriceMapSchemaOptionPrice;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Migrations\Migration;
-use Ramsey\Uuid\Uuid;
 
 return new class extends Migration
 {
@@ -14,35 +9,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $priceMaps = PriceMap::query()->get();
-
-        $query = Option::query();
-        /** @var PriceMap $priceMap */
-        foreach ($priceMaps as $priceMap) {
-            $query->orWhereDoesntHave('mapPrices', fn ($q) => $q->where('price_map_id', '=', $priceMap->getKey()));
-        }
-
-        $query->chunkById(100, function (Collection $options) use ($priceMaps) {
-            $options->each(function (Option $option) use ($priceMaps) {
-                $existingPrices = $option->mapPrices()->pluck('price_map_id')->toArray();
-                $data = [];
-                /** @var PriceMap $priceMap */
-                foreach ($priceMaps as $priceMap) {
-                    if (!in_array($priceMap->getKey(), $existingPrices)) {
-                        $data[] = [
-                            'id' => Uuid::uuid6(),
-                            'price_map_id' => $priceMap->getKey(),
-                            'option_id' => $option->getKey(),
-                            'value' => 0,
-                            'currency' => $priceMap->currency->value,
-                            'is_net' => $priceMap->is_net,
-                        ];
-                    }
-                }
-
-                PriceMapSchemaOptionPrice::query()->upsert($data, ['price_map_id', 'option_id']);
-            });
-        });
+        // removed
     }
 
     /**
@@ -50,6 +17,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        // removed
     }
 };
