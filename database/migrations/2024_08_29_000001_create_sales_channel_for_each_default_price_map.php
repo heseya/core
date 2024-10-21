b8a09a423af78984ec53ac932a7e6ca6a42e6d3b
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-10-21 12:14:27 +0200

    Fix migration

--- database/migrations/2024_08_29_000001_create_sales_channel_for_each_default_price_map.php
+++ database/migrations/2024_08_29_000001_create_sales_channel_for_each_default_price_map.php
@@ -1,11 +1,13 @@
 <?php
 
 use Domain\Currency\Currency;
+use Domain\Language\Language;
 use Domain\PriceMap\PriceMap;
 use Domain\SalesChannel\Enums\SalesChannelActivityType;
 use Domain\SalesChannel\Enums\SalesChannelStatus;
 use Domain\SalesChannel\Models\SalesChannel;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -16,12 +18,15 @@ return new class extends Migration
                 /** @var PriceMap $priceMap */
                 $priceMap = PriceMap::find($case->getDefaultPriceMapId());
                 if ($priceMap->salesChannels->count() === 0) {
-                    SalesChannel::factory()->create([
+                    SalesChannel::create([
                         'name' => 'Default channel for ' . $case->value,
                         'price_map_id' => $priceMap->id,
                         'status' => SalesChannelStatus::PRIVATE->value,
                         'activity' => SalesChannelActivityType::ACTIVE->value,
                         'default' => false,
+                        'slug' => Str::slug('Default channel for ' . $case->value),
+                        'language_id' => Language::default()->getKey(),
+                        'vat_rate' => '0',
                     ]);
                 }
             }
