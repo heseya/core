78ea90a8b0a8381f91a9b5c8adf554673c837a36
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-10-24 11:08:52 +0200

    More logs

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -37,13 +37,13 @@ class ProductResource extends ProductWithoutSalesResource
                 )
                 : [],
             'relevancy' => [
-                'title_position' => $this->resource->title_position, // @phpstan-ignore-line,
-                'title_relevancy' => $this->resource->title_relevancy, // @phpstan-ignore-line
-                'title_natural_relevancy' => $this->resource->title_natural_relevancy, // @phpstan-ignore-line
-                'title_words_relevancy' => $this->resource->title_words_relevancy, // @phpstan-ignore-line
-                'content_natural_relevancy' => $this->resource->content_natural_relevancy, // @phpstan-ignore-line
-                'content_relevancy' => $this->resource->content_relevancy, // @phpstan-ignore-line
-                'slug_length' => $this->resource->slug_length, // @phpstan-ignore-line
+                'title_position' => is_infinite($this->resource->title_position) || is_nan($this->resource->title_position) ? null : $this->resource->title_position, // @phpstan-ignore-line,
+                'title_relevancy' => is_infinite($this->resource->title_relevancy) || is_nan($this->resource->title_relevancy) ? null : $this->resource->title_relevancy, // @phpstan-ignore-line
+                'title_natural_relevancy' => is_infinite($this->resource->title_natural_relevancy) || is_nan($this->resource->title_natural_relevancy) ? null : $this->resource->title_natural_relevancy, // @phpstan-ignore-line
+                'title_words_relevancy' => is_infinite($this->resource->title_words_relevancy) || is_nan($this->resource->title_words_relevancy) ? null : $this->resource->title_words_relevancy, // @phpstan-ignore-line
+                'content_natural_relevancy' => is_infinite($this->resource->content_natural_relevancy) || is_nan($this->resource->content_natural_relevancy) ? 0 : $this->resource->content_natural_relevancy, // @phpstan-ignore-line
+                'content_relevancy' => is_infinite($this->resource->content_relevancy) || is_nan($this->resource->content_relevancy) ? 0 : $this->resource->content_relevancy, // @phpstan-ignore-line
+                'slug_length' => is_infinite($this->resource->slug_length) || is_nan($this->resource->slug_length) ? 0 : $this->resource->slug_length, // @phpstan-ignore-line
             ],
         ];
     }
